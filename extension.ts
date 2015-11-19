ce2c5262779a4dc7c9a24ebc004fe52c0806893a
Mahmoud Ali <muddibr@gmail.com>
2015-11-18 23:40:14 -0300

    Initial working version finished

--- extension.ts
+++ extension.ts
@@ -1,24 +1,106 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'; 
+import * as request from 'request';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+var selectedBrowsers = ['ie', 'firefox' , 'chrome', 'safari', 'opera'];
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-caniuse" is now active!'); 
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	var disposable = vscode.commands.registerCommand('extension.canIUse', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        var expandedSelection = undefined;
+        expandedSelection = getSelection(editor);
+        if (expandedSelection)
+        {
+            var word = editor.document.getText(expandedSelection);
+            word && retrieveInformation(word.toLowerCase(), expandedSelection);
+        }
 	});
 	
 	context.subscriptions.push(disposable);
+}
+
+function getSelection(editor: vscode.TextEditor): vscode.Selection {
+    const selection = editor.selection;
+    if (selection.isEmpty) {
+        const wordRange = editor.document.getWordRangeAtPosition(selection.active);
+        if (typeof wordRange != 'undefined') {
+            var expandedSelection = new vscode.Selection(wordRange.start.line, wordRange.start.character, wordRange.end.line, wordRange.end.character);
+            return expandedSelection;
+        } else {
+            return undefined;
+        }
+    } else {
+        return selection;
+    }
+}
+
+function retrieveInformation(word:string, selection: vscode.Selection)
+{
+    if (word)
+    {
+        request({
+            json: true,
+            uri: 'https://raw.githubusercontent.com/Fyrd/caniuse/master/data.json',
+            gzip: true
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var rule = body.data[word];
+                
+                if (rule && showInformation(rule))
+                {
+                    return;
+                }
+            }
+            
+            vscode.window.setStatusBarMessage("Can I Use: entry not found", 5000);
+        });
+    }
+}
+
+function showInformation(data) : boolean {
+    var stats = data.stats;
+    var result = [];
+    for (var i = 0; i < selectedBrowsers.length; i++) {
+        var browser = stats[selectedBrowsers[i]];
+        var version = getVersion(browser);
+        if (version)
+            result.push(selectedBrowsers[i] + ": "+ version);
+    }
+    
+    if (result && result.length > 0)
+    {
+        vscode.window.setStatusBarMessage("Can I Use: " + result.join(" | "), 5000);
+        return true;
+    }
+    
+    return false;
+}
+
+function getVersion(stats:any) : string {
+    var keys = Object.keys(stats).sort(function (a, b) {
+        var aNumber = +a;
+        var bNumber = +b;
+        return aNumber > bNumber ? 1 : aNumber == bNumber ? 0: -1 ;
+    });
+    
+    var found = [];
+    for (var i = 0; i < keys.length; i++) {
+        var element = keys[i];
+        if (stats[keys[i]].indexOf("a") >= 0 || stats[keys[i]].indexOf("y") >= 0)
+        {
+            found.push(keys[i]);
+        }
+    }
+    
+    var versions = [];
+    var first = found.shift();
+    var last = found.pop();
+    if (first)
+        versions.push(first);
+    if (last)
+        versions.push(last);
+    
+    return versions.join("-");
 }
---
+++ typings/tsd.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="form-data/form-data.d.ts" />
+/// <reference path="request/request.d.ts" />
