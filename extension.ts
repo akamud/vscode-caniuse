773465359da82087e4a44a6b83efb3c392e07d40
Mahmoud Ali <muddibr@gmail.com>
2015-11-26 21:32:50 -0300

    More typescript-like syntax

--- extension.ts
+++ extension.ts
@@ -1,110 +1,115 @@
 import * as vscode from 'vscode'; 
 import * as request from 'request';
 
-var selectedBrowsers = ['IE', 'Firefox' , 'Chrome', 'Safari', 'Opera'];
-
 export function activate(context: vscode.ExtensionContext) {
 	var disposable = vscode.commands.registerCommand('extension.canIUse', () => {
+        let caniuse = new CanIUse();
+        
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
         }
         var expandedSelection = undefined;
-        expandedSelection = getSelection(editor);
+        expandedSelection = caniuse.getSelection(editor);
         if (expandedSelection)
         {
             var word = editor.document.getText(expandedSelection);
-            word && retrieveInformation(word.toLowerCase(), expandedSelection);
+            word && caniuse.retrieveInformation(word.toLowerCase(), expandedSelection);
         }
 	});
 	
 	context.subscriptions.push(disposable);
 }
 
-function getSelection(editor: vscode.TextEditor): vscode.Selection {
-    const selection = editor.selection;
-    if (selection.isEmpty) {
-        const wordRange = editor.document.getWordRangeAtPosition(selection.active);
-        if (typeof wordRange != 'undefined') {
-            var expandedSelection = new vscode.Selection(wordRange.start.line, wordRange.start.character, wordRange.end.line, wordRange.end.character);
-            return expandedSelection;
-        } else {
-            return undefined;
-        }
-    } else {
-        return selection;
-    }
-}
-
-function retrieveInformation(word:string, selection: vscode.Selection)
-{
-    if (word)
-    {
-        request({
-            json: true,
-            uri: 'https://raw.githubusercontent.com/Fyrd/caniuse/master/data.json',
-            gzip: true
-        }, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var rule = body.data[word];
-                
-                if (rule && showInformation(rule))
-                {
-                    return;
-                }
+export class CanIUse {
+    private  selectedBrowsers = ['IE', 'Firefox' , 'Chrome', 'Safari', 'Opera'];
+    
+    getSelection(editor: vscode.TextEditor): vscode.Selection {
+        const selection = editor.selection;
+        if (selection.isEmpty) {
+            const wordRange = editor.document.getWordRangeAtPosition(selection.active);
+            if (typeof wordRange != 'undefined') {
+                var expandedSelection = new vscode.Selection(wordRange.start.line, wordRange.start.character, wordRange.end.line, wordRange.end.character);
+                return expandedSelection;
+            } else {
+                return undefined;
             }
-            
-            vscode.window.setStatusBarMessage("Can I Use: entry not found", 5000);
-        });
-    }
-}
-
-function showInformation(data) : boolean {
-    var stats = data.stats;
-    var result = [];
-    for (var i = 0; i < selectedBrowsers.length; i++) {
-        var browser = stats[selectedBrowsers[i].toLowerCase()];
-        var version = getVersion(browser);
-        if (version)
-        {
-            result.push(selectedBrowsers[i] + " ✔ " + version);
+        } else {
+            return selection;
         }
-        else
-        {
-            result.push(selectedBrowsers[i] + " ✘");
-        }
-            
     }
     
-    if (result && result.length > 0)
+    retrieveInformation(word:string, selection: vscode.Selection)
     {
-        vscode.window.setStatusBarMessage("Can I Use: " + result.join(" "), 5000);
-        return true;
-    }
-    
-    return false;
-}
-
-function getVersion(stats:any) : string {
-    var keys = Object.keys(stats).sort(function (a, b) {
-        var aNumber = +a;
-        var bNumber = +b;
-        return aNumber > bNumber ? 1 : aNumber == bNumber ? 0: -1 ;
-    });
-    
-    var found = [];
-    for (var i = 0; i < keys.length; i++) {
-        var element = keys[i];
-        if (stats[keys[i]].indexOf("a") >= 0 || stats[keys[i]].indexOf("y") >= 0)
+        if (word)
         {
-            found.push(keys[i]);
-            break;
+            let caniuse = this;
+            request({
+                json: true,
+                uri: 'https://raw.githubusercontent.com/Fyrd/caniuse/master/data.json',
+                gzip: true
+            }, (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    var rule = body.data[word];
+                    
+                    if (rule && caniuse.showInformation(rule))
+                    {
+                        return;
+                    }
+                }
+                
+                vscode.window.setStatusBarMessage("Can I Use: entry not found", 5000);
+            });
         }
     }
     
-    var versions = [];
-    var first = found.shift();
-    first && versions.push(first + "+");
+    showInformation(data): boolean {
+        var stats = data.stats;
+        var result = [];
+        for (var i = 0; i < this.selectedBrowsers.length; i++) {
+            var browser = stats[this.selectedBrowsers[i].toLowerCase()];
+            var version = this.getVersion(browser);
+            if (version)
+            {
+                result.push(this.selectedBrowsers[i] + " ✔ " + version);
+            }
+            else
+            {
+                result.push(this.selectedBrowsers[i] + " ✘");
+            }
+                
+        }
+        
+        if (result && result.length > 0)
+        {
+            vscode.window.setStatusBarMessage("Can I Use: " + result.join(" "), 5000);
+            return true;
+        }
+        
+        return false;
+    }
     
-    return versions.join("-");
+    getVersion(stats:any): string {
+        var keys = Object.keys(stats).sort(function (a, b) {
+            var aNumber = +a;
+            var bNumber = +b;
+            return aNumber > bNumber ? 1 : aNumber == bNumber ? 0: -1 ;
+        });
+        
+        var found = [];
+        for (var i = 0; i < keys.length; i++) {
+            var element = keys[i];
+            if (stats[keys[i]].indexOf("a") >= 0 || stats[keys[i]].indexOf("y") >= 0)
+            {
+                found.push(keys[i]);
+                break;
+            }
+        }
+        
+        var versions = [];
+        var first = found.shift();
+        first && versions.push(first + "+");
+        
+        return versions.join("-");
+    }
 }
